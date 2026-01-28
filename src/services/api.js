31114b3a9748bf79d942c0bd45d61a561a52363c
Sowshild <sowshild@gmail.com>
2026-01-28 19:30:20 +0530

    Added Headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'import.meta.env.VITE_API_URL',
+    baseURL: 'import.meta.env.VITE_API_URL', 
+    headers: {"Content-Type": "application/json"},
 });
 
 // Add a request interceptor to add the auth token to headers
